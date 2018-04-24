6f03e5ef00f9afadfe70f7374004098ecf649d13
darceygonzalez25 <darceygonzalez25@live.com>
2018-04-23 23:41:43 -0500

    minor css changes

--- config/connection.js
+++ config/connection.js
@@ -7,10 +7,9 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-    port: 8889,
+    port: 3306,
     host: "localhost",
     user: "root",
-    password: "root",
     database: "restaurants_db"
   });
 };
